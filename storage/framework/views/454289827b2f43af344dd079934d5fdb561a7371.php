bfa941ec89e7251cb023b24d610e81fcaf928546
hiral-vh <hiraldesani.vhits@gmail.com>
2024-05-27 14:33:33 +0530

    first commit

--- storage/framework/views/454289827b2f43af344dd079934d5fdb561a7371.php
+++
@@ -1,7 +0,0 @@
-<?php echo $__env->make('admin.layouts.header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<?php echo $__env->make('admin.layouts.topbar', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<?php echo $__env->make('admin.layouts.sidebar', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-
-<?php echo $__env->yieldContent('content'); ?>
-
-<?php echo $__env->make('admin.layouts.footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\e_food\resources\views/admin/master.blade.php ENDPATH**/ ?>
